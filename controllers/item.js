b825f5f945de18e6ecfc4059b4358f1ef511da76
Juan Navarro <jdna0615@gmail.com>
2018-05-27 00:19:58 -0400

    merge a desarrollo de items

--- controllers/item.js
+++ controllers/item.js
@@ -12,7 +12,7 @@ function getItems (req, res) {
 		var id = req.query.id;
 		console.log(req.query);
 		console.log(id);
-		res.send('uno');
+		res.send('obtener uno');
 	} else {
 		var sql = "select * from items";
 		connection.query(sql, function (err, result) {
