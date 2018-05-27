2ca433068b147e6e71b1828ad708de90210e63aa
Juan Navarro <jdna0615@gmail.com>
2018-05-27 00:10:34 -0400

    merge a desarrollo de items

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,18 @@ const auth = require('../middlewares/auth')
 
 const api = express.Router()
 
-/*
+/**
 * Modulo items
-*/
-
+**/
 api.get('/items', auth, itemCtrl.getItems)
+api.get('/items/:id', auth, itemCtrl.getItem)
+api.post('/items', auth, itemCtrl.insertItem)
+api.delete('/items/:id', auth, itemCtrl.deleteItem)
+api.put('/items', auth, itemCtrl.updateItem)
 
-/*
+/**
 * Modulo usuarios
-*/
+**/
 api.post('/signup', userCtrl.signUp)
 api.post('/signin', userCtrl.signIn)
 api.get('/users', userCtrl.getUsers)
